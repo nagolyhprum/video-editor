7c3750571b3394389461e6fe0732d27b6694ecb5
Logan Murphy <nagolyhprum@hotmail.com>
2023-08-05 12:54:29 -0400

    show better time state

--- public/components/stats.js
+++ public/components/stats.js
@@ -1,15 +1,19 @@
 {
     const statsDiv = document.querySelector('#stats');
 
+    const toTime = (input) => {
+        const minutes = Math.floor(input / 60)
+        const seconds = Math.floor(input % 60)
+        return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+    }
+
     state.watch(["time", "timeline"], (time, timeline) => {
         const result = getActiveClip();
         if(result) {
             const length = timeline.reduce((length, clip) => length + clip.length, 0)
-            const minutes = Math.floor(length / 60)
-            const seconds = Math.floor(length % 60)
             const json = {
-                time,
-                length : `${minutes}:${seconds.toString().padStart(2, "0")}`,
+                time: toTime(time),
+                length : toTime(length),
                 start : result.start,
                 media : result.clip.media.length
             }
